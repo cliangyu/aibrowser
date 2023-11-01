575fe85be11886595d506dc7f1e9de41ac33e912
cliangyu <chenliangyudavid@gmail.com>
2023-11-01 10:21:25 +0800

    transfer to class

--- call_js.py
+++ call_js.py
@@ -1,15 +1,19 @@
 import subprocess
 
-def call_javascript(imgpath, text):
-    try:
-        result = subprocess.run(["node", "test.js", imgpath, text], capture_output=True, text=True, check=True)
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print("Error:", e)
-        print(e.stderr)
+class GPT4v():
+    def __init__(self,imgpath,text) -> None:
+        self.imgpath = imgpath
+        self.text = text
+    def call_javascript(self):
+        try:
+            result = subprocess.run(["node", "test.js", self.imgpath, self.text], capture_output=True, text=True, check=True)
+            print(result.stdout)
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
+            print(e.stderr)
 
 if __name__ == "__main__":
-    imgpath = "/data/lychen/code/web/ai-browser/images/building.jpg"
-    text = "what is in the image?"
-    
-    call_javascript(imgpath,text)
+    imgpath = "/data/lychen/code/web/ai-browser/output_image.jpg"
+    text = "what's in the image"
+    a = GPT4v(imgpath,text)
+    a.call_javascript()
